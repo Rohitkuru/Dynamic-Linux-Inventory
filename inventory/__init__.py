6aa9654a743329188a2dcd8759e816bf28719836
Rohitkuru <56543840+Rohitkuru@users.noreply.github.com>
2021-03-12 12:36:25 +0530

    Create __init__.py

---
+++ inventory/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///server_inventory_bootstrap.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+
+from inventory.home.view import home
+
+app.register_blueprint(home)
