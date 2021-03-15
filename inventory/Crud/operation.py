0cc1b0c4ebc9edb35b2ba64b51f451d36af87304
Administrator <rohit.kurdukar@siemens.com>
2021-03-15 06:59:49 +0000

    added db operation and chaned home view from database instead of using list

--- inventory/Crud/operation.py
+++ inventory/Crud/operation.py
@@ -5,18 +5,28 @@ from inventory import db
 
 def add_record(linux_host,range):
 
-    if not Networkscope.query.filter_by(cidr=range).first():
-        cidr_record = Networkscope(cidr=range)
-        db.session.add(cidr_record)
-        db.session.commit()
-        logging.info("Record has been added to database, record is {}".format(cidr_record))
+    try:
+        if not Networkscope.query.filter_by(cidr=range).first():
+            cidr_record = Networkscope(cidr=range)
+            db.session.add(cidr_record)
+            db.session.commit()
+            logging.info("Record has been added to database, record is {}".format(cidr_record))
+        else:
+            logging.info("CIDR {} already exist in database ".format(range))
 
-    for each in linux_host:
-        host_record = Linux_inventory(IPAddress=each['ip'], hostname=each['hostname'],
-                        uptime=each['uptime'],
-                        operating_system=each['cat /etc/redhat-release'],
-                        last_time_checked=each['last_time_checked'], last_status_check=each['last_status'],
-                        networkscope_cidr=range)
-        db.session.add(host_record)
-        db.session.commit()
-        logging.info("Record has been added in database , record is {} ".format(host_record))
+        for each in linux_host:
+            if not Linux_inventory.query.filter_by(IPAddress=each['ip']).first():
+                host_record = Linux_inventory(IPAddress=each['ip'], hostname=each['hostname'],
+                                    uptime=each['uptime'],
+                                    operating_system=each['cat /etc/redhat-release'],
+                                    last_time_checked=each['last_time_checked'], last_status_check=each['last_status'],
+                                    networkscope_cidr=range)
+                db.session.add(host_record)
+                db.session.commit()
+                logging.info("Record has been added in database , record is {} ".format(host_record))
+            else:
+               logging.info("Record already  in database .. {} , hence skipping ".format(each['ip']))
+
+    except Exception as e:
+        db.session.rollback()
+        logging.error(" add_record operation failed , due to exception occured .. {}".format(e))
--- inventory/home/view.py
+++ inventory/home/view.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,render_template,request,flash
 from inventory.backend.scripts import scan
 from inventory.Crud.operation import add_record
+from inventory.models import *
 
 
 home = Blueprint("home_view",__name__)
@@ -12,8 +13,9 @@ def home_view():
         if type(find_result) == list:
             flash("Search completed and Inventory updated")
             add_record(find_result,request.form['range'])
-            return render_template("home.html",find_result=find_result)
+            return render_template("home.html",find_result=Linux_inventory.query.all())
         else:
             flash(find_result)
-            return render_template("home.html")
-    return render_template("home.html")
+            return render_template("home.html",find_result=Linux_inventory.query.all())
+
+    return render_template("home.html",find_result=Linux_inventory.query.all())
